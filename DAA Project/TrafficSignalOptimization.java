5711ed79c64f78e670ced35cc394cc8d33e0ac51
VishalShrivastava01 <vishalshrivastava344@gmail.com>
2026-04-21 14:10:00 +0530

    Add files via upload

---
+++ DAA Project/TrafficSignalOptimization.java
@@ -0,0 +1,68 @@
+// Traffic signal optimization project.......
+
+
+import java.util.*;
+
+class TrafficSignalOptimization {
+
+    static final int V = 5; 
+
+    int minDistance(int dist[], boolean visited[]) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = -1;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i] && dist[i] < min) {
+                min = dist[i];
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
+    void dijkstra(int graph[][], int source) {
+        int dist[] = new int[V];
+        boolean visited[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            visited[i] = false;
+        }
+
+        dist[source] = 0;
+
+        for (int count = 0; count < V - 1; count++) {
+            int u = minDistance(dist, visited);
+            visited[u] = true;
+
+            for (int v = 0; v < V; v++) {
+                if (!visited[v] && graph[u][v] != 0 &&
+                        dist[u] != Integer.MAX_VALUE &&
+                        dist[u] + graph[u][v] < dist[v]) {
+
+                    dist[v] = dist[u] + graph[u][v];
+                }
+            }
+        }
+
+        System.out.println("Junction \t Minimum Time");
+        for (int i = 0; i < V; i++) {
+            System.out.println(i + "\t\t" + dist[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int graph[][] = {
+                {0, 10, 0, 30, 100},
+                {10, 0, 50, 0, 0},
+                {0, 50, 0, 20, 10},
+                {30, 0, 20, 0, 60},
+                {100, 0, 10, 60, 0}
+        };
+
+        TrafficSignalOptimization t = new TrafficSignalOptimization();
+        t.dijkstra(graph, 0);
+    }
+}
